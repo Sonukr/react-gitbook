8b0f50deaf2df87dc553afc264ad2202231e0992
Susi Remondi <susi@ga.co>
2017-04-05 11:26:18 -0400

    Fix: Authorlist should be author

--- projects/project-03-state/solution-code/src/Author.js
+++ projects/project-03-state/solution-code/src/Author.js
@@ -4,7 +4,7 @@ class Author extends Component {
   render () {
     return (
       <div>
-        <p>Written by {this.props.authorList}</p>
+        <p>Written by {this.props.author}</p>
       </div>
     )
   }
